d16703c79069329816867a39554f981413efbf18
AntoCic <anto.cic.127@gmail.com>
2024-03-18 13:26:47 +0100

    MILESTONE 0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 console.log("- START -");
+
+let users = [];
+
+users.push(addNewUser("Wayne Barnett", "Founder & CEO", "wayne-barnett-founder-ceo.jpg"));
+users.push(addNewUser("Angela Caroll", "Chief Editor", "angela-caroll-chief-editor.jpg"));
+users.push(addNewUser("Walter Gordon", "Office Manager", "walter-gordon-office-manager.jpg"));
+users.push(addNewUser("Angela Lopez", "Social Media Manager", "angela-lopez-social-media-manager.jpg"));
+users.push(addNewUser("Scott Estrada", "Developer", "scott-estrada-developer.jpg"));
+users.push(addNewUser("Barbara Ramos", "Graphic Designer", "barbara-ramos-graphic-designer.jpg"));
+
+console.log(users);
+
+function addNewUser(name, rule, picture) {
+    return { 
+        name : name,
+        rule : rule,
+        picture : picture
+    }
+}
